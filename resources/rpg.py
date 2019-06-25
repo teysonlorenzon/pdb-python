7df5ba530f6aa549eac709ebfd2cc58e1d7eb4d3
Teyson Lorenzon <teyson.lorenzon_BOLS@pampa.compasso>
2019-06-25 10:36:56 -0300

    atividade 02: start - menu principal
    criado menu principal para o usuário, faltando o input de inicalização de escolhas do jogo

---
+++ resources/rpg.py
@@ -0,0 +1,24 @@
+#menu Principal
+
+quer_jogar = input("Deseja iniciar o jogo King's Game? (sim/nao): ")
+
+
+if(quer_jogar.lower() == "sim"):
+    print("******************************************************************************************************")
+    print("*                                            KING'S Game                                             *")
+    print("******************************************************************************************************")
+    print("*                                                                                                    *")
+    print("*                       Escolha o numero referente a quatidade de jogadores                          *")
+    print("*                                                                                                    *")
+    print("*                               (1): 2 Jogadores  (3): 4 Jogadores                                   *")
+    print("*                               (2): 3 Jogadores  (4): 5 Jogadores                                   *")
+    print("*                                                                                          (5): Sair *")
+    print("******************************************************************************************************")
+
+
+
+
+
+
+
+
