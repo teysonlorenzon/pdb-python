fea732042cfab1216bb003ff8b9ec3c928cafbca
Teyson Lorenzon <teyson.lorenzon_BOLS@pampa.compasso>
2019-06-25 10:56:40 -0300

    atividade 02: start - receber nome do jogador
    criado opções referente a escolha de jogadores pelo usuario, e solicitado os nomes para cada jogador da partida

--- resources/rpg.py
+++ resources/rpg.py
@@ -10,15 +10,38 @@ if(quer_jogar.lower() == "sim"):
     print("*                                                                                                    *")
     print("*                       Escolha o numero referente a quatidade de jogadores                          *")
     print("*                                                                                                    *")
-    print("*                               (1): 2 Jogadores  (3): 4 Jogadores                                   *")
-    print("*                               (2): 3 Jogadores  (4): 5 Jogadores                                   *")
-    print("*                                                                                          (5): Sair *")
+    print("*                               (2): 2 Jogadores  (4): 4 Jogadores                                   *")
+    print("*                               (3): 3 Jogadores  (5): 5 Jogadores                                   *")
+    print("*                                                                                          (1): Sair *")
     print("******************************************************************************************************")
 
 
+    numero_jogadores = int(input("Digite o numero referente a quantidade de jogadores (2 a 5) (1 para sair): "))
+    while (numero_jogadores < 1 or numero_jogadores > 5):
+        numero_jogadores = int(input("Respeite os numeros do menu (1 a 5): "))
+
+    jogadores = []
+
+    if (numero_jogadores == 1):
+        print("Jogo encerrado!")
+
+    elif (numero_jogadores == 2):
+        for i in range(1,3):
+            jogador = input("Jogador {0}: ".format(i))
+            jogadores.append(jogador)
+
+    elif (numero_jogadores == 3):
+        for i in range(1,4):
+            jogador = input("Jogador {0}: ".format(i))
+            jogadores.append(jogador)
 
 
+    elif (numero_jogadores == 4):
+        for i in range(1,5):
+            jogador = input("Jogador {0}: ".format(i))
+            jogadores.append(jogador)
 
-
-
-
+    elif (numero_jogadores == 5):
+        for i in range(1,6):
+            jogador = input("Jogador {0}: ".format(i))
+            jogadores.append(jogador)
